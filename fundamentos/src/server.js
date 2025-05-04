8559a24c463faa38f46b6e0f92aadb65dcb13962
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-05-03 22:35:50 -0300

    Utilizadno o status code

--- fundamentos/src/server.js
+++ fundamentos/src/server.js
@@ -2,7 +2,7 @@ import http from "node:http"
 
 const server = http.createServer((req, res) => {
     const {method} = req
-    return res.end("Metodo: " + method)
+    return res.writeHead(200).end("Metodo: " + method)
 })
 
 server.listen(3333)
