0b554509bc96f070a377912601534c3da0cb6f16
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-05-03 22:45:43 -0300

    URL da requisição

--- fundamentos/src/server.js
+++ fundamentos/src/server.js
@@ -1,8 +1,16 @@
 import http from "node:http"
 
 const server = http.createServer((req, res) => {
-    const {method} = req
-    return res.writeHead(200).end("Metodo: " + method)
+    const {method, url} = req
+    
+    if(method === "GET" && url === "/products"){
+        return res.end("Lista de produtos!")    
+    }
+
+    if (method === "POST" && url === "/products"){
+        return res.writeHead(201).end("Produtos cadastrado!")
+    }
+    return res.writeHead(404).end("Rota nao encontrada")
 })
 
 server.listen(3333)
