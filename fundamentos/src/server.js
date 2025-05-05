c8a730cbaf2c68e0fd1fa09fe60ea5b09f50e33b
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-05-05 19:48:36 -0300

    Enviar e recuperardados no body

--- fundamentos/src/server.js
+++ fundamentos/src/server.js
@@ -1,13 +1,21 @@
 import http from "node:http"
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const {method, url} = req
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    console.log(Buffer.concat(buffers).toString())
     
     if(method === "GET" && url === "/products"){
         return res.end("Lista de produtos!")    
     }
 
     if (method === "POST" && url === "/products"){
+
         return res.writeHead(201).end("Produtos cadastrado!")
     }
     return res.writeHead(404).end("Rota nao encontrada")
