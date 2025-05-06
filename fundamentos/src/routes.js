fdef82cd919f9e11cb51631f4ffc2af6686469bd
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-05-05 21:43:11 -0300

    Separando as rotas

---
+++ fundamentos/src/routes.js
@@ -0,0 +1,17 @@
+export const routes = [
+  {
+    method: "GET",
+    path: "/products",
+    controller: (req, res) => {
+      return res.end("Lista de produtos!");
+    },
+  },
+
+  {
+    method: "POST",
+    path: "/products",
+    controller: (req, res) => {
+      return res.writeHead(201).end(JSON.stringify(req.body));
+    },
+  },
+];
--- fundamentos/src/server.js
+++ fundamentos/src/server.js
@@ -1,20 +1,10 @@
 import http from "node:http"
 import { jsonBodyHanndler } from "./middlewares/jsonBodyHanndler.js"
+import { routeHander } from "./middlewares/routeHandller.js"
 
 const server = http.createServer(async (req, res) => {
-    const {method, url} = req
-
     await jsonBodyHanndler(req, res)
-
-    if(method === "GET" && url === "/products"){
-        return res.end("Lista de produtos!")    
-    }
-
-    if (method === "POST" && url === "/products"){
-        console.log(req.body)
-        return res.writeHead(201).end(JSON.stringify(req.body))
-    }
-    return res.writeHead(404).end("Rota nao encontrada")
+    routeHander(req, res)
 })
 
 server.listen(3333)
