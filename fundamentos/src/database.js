ef44999fe0181cb11da621c39f5ffb35ca88e4ca
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-06-26 22:56:29 -0300

    Lendo dados no arquivo

--- fundamentos/src/database.js
+++ fundamentos/src/database.js
@@ -1,28 +1,33 @@
 import fs from "node:fs/promises";
+
 const DATABASE_PATH = new URL("db.json", import.meta.url);
 export class Database {
-  database = {};
+  #database = {};
 
   constructor() {
-    this.persist();
+    fs.readFile(DATABASE_PATH, "utf-8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => this.#persist());
   }
 
-  persist() {
-    fs.writeFile(DATABASE_PATH, JSON.stringify(this.database))
+  async #persist() {
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database));
   }
 
   // se tiver a tabela so insere caso cosontrario cria
   insert(table, data) {
-    if (Array.isArray(this.database[table])) {
-      this.database[table].push(data);
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
     } else {
-      this.database[table] = [data];
+      this.#database[table] = [data];
     }
 
-    this.persist()
+    this.#persist();
   }
 
   select(tabela) {
-    return this.database[tabela];
+    return this.#database[tabela] ?? [];
   }
 }
